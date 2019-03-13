56a42fa4749b982b474cdaf86b76d750887ccf4c
Szelethus <dkszelethus@gmail.com>
2019-03-13 22:54:26 +0100

    Drastically simplify the tblgen file

--- lib/StaticAnalyzer/Checkers/DebugCheckers.cpp
+++ lib/StaticAnalyzer/Checkers/DebugCheckers.cpp
@@ -231,7 +231,7 @@ public:
                    << (Keys[I]->second.empty() ? "\"\"" : Keys[I]->second)
                    << '\n';
 
-#define ANALYZER_FRONTEND_OPTION(PREFIX, NAME, ID, ALIAS, HELPTEXT) \
+#define ANALYZER_FRONTEND_OPTION(NAME, ID, HELPTEXT) \
     llvm::errs() << NAME << '\n';
 
 #include "clang/StaticAnalyzer/Frontend/Options.def"
